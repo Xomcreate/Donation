64243683283511da6ab57e0c826c805298417ed4
Xomcreate <priscaojimba15@gmail.com>
2025-01-09 08:16:11 +0100

    Update

--- src/DonationComponent/SignUp.jsx
+++ src/DonationComponent/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Signin from "./Signin";
+import SignIn from "./Signin";
 
 function SignUp() {
   const [showSignIn, setShowSignIn] = useState(false);
